35f7fb88869a268da6399f9c8abd9a7e009924fa
Gustavo Kendy de Souza Kishima <gustavokishima@gmail.com>
2013-05-11 02:21:11 -0300

    main2 teste e entrada de teste
    Arquivos para teste rápido das funç~es

---
+++ main2.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+public class main2{
+	public static void main(String args[]){
+		System.out.println("Default Text Readaer 1.0");
+		System.out.println("Digite o nome do arquivo que deseja buscar:\n");
+		File f = new File("entrada.txt");
+		BuscaPalavra novo = new BuscaPalavra(f);
+		String wurd = "you";
+		System.out.println("Ocorrencias de "+wurd+": "+novo.numeroDeOcorrencias(wurd));
+		System.out.println("Ocorrencias de "+wurd+": "+novo.buscaIndice(wurd));
+		wurd = "love";
+		System.out.println("Ocorrencias de "+wurd+": "+novo.numeroDeOcorrencias(wurd));
+		System.out.println("Ocorrencias de "+wurd+": "+novo.buscaIndice(wurd));
+		novo.linhas.imprimeLista();
+		
+	}
+
+
+}
