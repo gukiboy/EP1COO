ba2a821cb1f901cb570fb511dcaacd922834ef31
gukiboy <gustavokishima@gmail.com>
2013-05-20 17:37:07 -0300

    Update Palavra.java
    Arquivo agora é atributo da classe.
    Métodos corrigidos para arrumar.

--- Palavra.java
+++ Palavra.java
@@ -2,21 +2,44 @@ class Palavra{
     private int numeroDaLinhaDaPalavra;
     private String fraseDaPalavra;
     private String palavra;
+    private String arquivoDaPalavra;
     private Palavra proximo;
 
-    public Palavra(int num, String frase, String palavra){
+    public Palavra(int num, String frase, String palavra,String arquivo){
         this.numeroDaLinhaDaPalavra = num
         this.fraseDaPalavra = frase;
         this.palavra = palavra;
+      this.arquivoDaPalavra = arquivo;
     }
 
-    void setPalavra(int num, String frase, String palavra){
+    void setPalavra(int num, String frase, String palavra,String arquivo){
         this.numeroDaLinhaDaPalavra = num
         this.fraseDaPalavra = frase;
         this.palavra = palavra;
+	  this.arquivoDaPalavra = arquivo;
     }//reconfigura o objeto, redefinindo os atributos
 
     Palavra getProximo(){ //retorna o proximo membro da lista
         return this.proximo;
+    }
+
+    boolean temProximo(){//identifica se ha alguma palavra alem dessa
+	return (proximo!=NULL);
+    }
+
+    String getPalavra(){
+	return this.palavra;
+    }
+
+    String getFrase(){
+	return this.fraseDaPalavra;
+    }
+
+    String getArquivo(){
+	return this.arquivoDaPalavra;
+    }
+
+    int getNumero(){
+	return this.numeroDaLinhaDaPalavra;
     }
 }
